e4b6054845f3af64e763ffc4ea75e0b0add64112
Dev <dev.rangarajan@gmail.com>
2016-10-12 19:16:34 -0700

    Added logging mockup, package structure

---
+++ TeamCode/src/main/java/org/ftcteam5206/utils/DataLogger.java
@@ -0,0 +1,16 @@
+package org.ftcteam5206.utils;
+
+/**
+ * Created by Dev on 10/12/2016.
+ */
+
+public class DataLogger {
+    //TODO: Single logging function that writes to debug, optionally telemetry and txt file on phone.
+    //TODO: See if this needs its own thread.
+
+    public static void dataLog(String s, Object o){
+        throw new UnsupportedOperationException();
+        //TODO: Add write logic.
+    }
+
+}
