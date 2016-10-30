ee529a19c636e14e679da794abe66860ee3159bf
Cat Y <catherinehyeo@gmail.com>
2016-10-30 13:58:56 -0700

    Added code for logging
    Logs to internal memory using openFileOutput

--- TeamCode/src/main/java/org/ftcteam5206/utils/DataLogger.java
+++ TeamCode/src/main/java/org/ftcteam5206/utils/DataLogger.java
@@ -4,6 +4,8 @@ package org.ftcteam5206.utils;
  * Created by Dev on 10/12/2016.
  */
 
+import java.io.*;
+
 public class DataLogger {
     //TODO: Single logging function that writes to debug, optionally telemetry and txt file on phone.
     //TODO: See if this needs its own thread.
@@ -11,6 +13,14 @@ public class DataLogger {
     public static void dataLog(String s, Object o){
         throw new UnsupportedOperationException();
         //TODO: Add write logic.
+        
+        try {
+            FileOutputStream fOut = openFileOutput("file name here", 1);
+            OutputStreamWriter writer = new OutputStreamWriter(fOut);
+            writer.write("something");
+
+        } catch (Exception e) {
+        }
     }
 
 }
