fcd7cb01e6a5a521006cef237c08ccc9606e6422
Cat Y <catherinehyeo@gmail.com>
2016-10-30 17:24:44 -0700

    Edit data logger
    Fixes bug

--- TeamCode/src/main/java/org/ftcteam5206/utils/DataLogger.java
+++ TeamCode/src/main/java/org/ftcteam5206/utils/DataLogger.java
@@ -4,22 +4,24 @@ package org.ftcteam5206.utils;
  * Created by Dev on 10/12/2016.
  */
 
+import android.content.Context;
+
 import java.io.*;
 
 public class DataLogger {
     //TODO: Single logging function that writes to debug, optionally telemetry and txt file on phone.
     //TODO: See if this needs its own thread.
 
-    public static void dataLog(String s, Object o){
+    public static void dataLog(String s, Object o, Context c){
         throw new UnsupportedOperationException();
         //TODO: Add write logic.
         
         try {
-            FileOutputStream fOut = openFileOutput("file name here", 1);
+            FileOutputStream fOut = c.openFileOutput("file name here", 1);
             OutputStreamWriter writer = new OutputStreamWriter(fOut);
-            writer.write("something");
-
-        } catch (Exception e) {
+            writer.write(s);
+        } catch(Exception e){
+            e.printStackTrace();
         }
     }
 
