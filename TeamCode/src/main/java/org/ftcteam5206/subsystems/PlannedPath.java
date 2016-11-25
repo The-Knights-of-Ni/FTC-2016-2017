c7c46a308232815892a123c65514a3ddc293bee3
Dev <dev.rangarajan@gmail.com>
2016-11-25 15:28:31 -0800

    Started on 1D Motion Planning

---
+++ TeamCode/src/main/java/org/ftcteam5206/subsystems/PlannedPath.java
@@ -0,0 +1,14 @@
+package org.ftcteam5206.subsystems;
+
+/**
+ * Created by Dev on 11/25/2016.
+ */
+
+public class PlannedPath {
+    /*
+        Take path length in, as well as max velocity, max acceleration, and max jerk.
+        Update function is called with encoder feedback, returns motor pwm
+        TODO: Add stabilization based on gyro heading (return 2d vector of left/right)
+         */
+
+}
